7af7edd79041d0b20fb6d160df37245df6531969
Razorged <nthieupraha@gmail.com>
2024-10-06 18:26:17 +0200

    changed powerup behaviour

--- LudumDare56/Assets/Scripts/PowerupScript.cs
+++ LudumDare56/Assets/Scripts/PowerupScript.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class PowerupScript : MonoBehaviour
 {
+    public GameObject mergePowerup;
+    public GameObject unmergePowerup;
     public MergeGunScript mergeGunScript;
     public UnmergeGunScript unmergeGunScript;
     // Start is called before the first frame update
@@ -21,13 +23,32 @@ public class PowerupScript : MonoBehaviour
 
     public void EquipMergeGun()
     {
+        if(unmergeGunScript.enabled == true)
+        {
+            SpawnUnmergePowerup();
+        }
         mergeGunScript.enabled = true;
         unmergeGunScript.enabled = false;
 
     }
     public void EquipUnMergeGun()
     {
+        if(mergeGunScript.enabled == true)
+        {
+            SpawnMergePowerup();
+        }
         mergeGunScript.enabled = false;
         unmergeGunScript.enabled = true;
+    }
+
+    private void SpawnMergePowerup()
+    {
+        mergePowerup.GetComponent<MergePowerupScript>().primed = false;
+        Instantiate(mergePowerup, transform.position, Quaternion.identity);
+    }
+    private void SpawnUnmergePowerup()
+    {
+        unmergePowerup.GetComponent<UnmergePowerupScript>().primed = false;
+        Instantiate(unmergePowerup, transform.position, Quaternion.identity);
     }
 }
