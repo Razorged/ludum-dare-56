ffd5c11244f492081a064401a555ce7a3e9a4a42
ohalouzka <155528064+ohalouzka@users.noreply.github.com>
2024-10-07 20:19:48 +0100

    ui to "LevelOne" only

---
+++ LudumDare56/Assets/Scripts/MainMenuButtonScript.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuButtonScript : MonoBehaviour
+{
+
+    public GameObject noneEquippedPanel;
+    public GameObject glueGunEquippedPanel;
+    public GameObject unglueGunEquippedPanel;
+
+    public void GoToScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void QuitApp()
+    {
+        Application.Quit();
+        Debug.Log("quit");
+    }
+
+    public void RestartScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void GoToMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void setGlueGunActive()
+    {
+        noneEquippedPanel.SetActive(false);
+        unglueGunEquippedPanel.SetActive(false);
+        glueGunEquippedPanel.SetActive(true);
+    }
+
+    public void setUnglueGunActive()
+    {
+        noneEquippedPanel.SetActive(false);
+        unglueGunEquippedPanel.SetActive(true);
+        glueGunEquippedPanel.SetActive(false);
+    }
+}
